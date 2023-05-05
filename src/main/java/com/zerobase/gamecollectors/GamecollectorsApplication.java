9593266183df28a22581bca46ae3b71e311a7392
KIM <chk7119@naver.com>
2023-05-06 00:08:40 +0900

    Initial Commit

---
+++ src/main/java/com/zerobase/gamecollectors/GamecollectorsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.gamecollectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecollectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecollectorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/gamecollectors/GamecollectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.gamecollectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecollectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
