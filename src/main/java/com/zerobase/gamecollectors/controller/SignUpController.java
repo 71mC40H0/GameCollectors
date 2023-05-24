6e2c5c3454f29caab452eb437b873a1e1450cc95
KIM <chk7119@naver.com>
2023-05-24 15:49:10 +0900

    refactor : code review에 따른 enum으로 응답관리를 위한 response code 클래스 추가 및 @Email 검증을 위한 @Valid 추가

--- src/main/java/com/zerobase/gamecollectors/controller/SignUpController.java
+++ src/main/java/com/zerobase/gamecollectors/controller/SignUpController.java
@@ -1,9 +1,11 @@
 package com.zerobase.gamecollectors.controller;
 
 import com.zerobase.gamecollectors.model.ManagerSignUpRequestDto;
+import com.zerobase.gamecollectors.response.ResponseCode;
 import com.zerobase.gamecollectors.service.ManagerSignUpService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/signUp")
+@RequestMapping("/sign-up")
 @RequiredArgsConstructor
 public class SignUpController {
 
@@ -23,27 +25,30 @@ public class SignUpController {
 
     @ApiOperation(value = "관리자 회원 가입")
     @PostMapping("/manager")
-    public ResponseEntity<String> managerSignUp(
-        @RequestBody @ApiParam(value = "관리자 회원가입 양식") ManagerSignUpRequestDto request) {
+    public ResponseEntity<?> managerSignUp(
+        @RequestBody @ApiParam(value = "관리자 회원가입 양식") @Valid ManagerSignUpRequestDto request) {
         managerSignUpService.signUp(request.toServiceDto());
-        return ResponseEntity.ok("회원가입이 완료되었습니다. 이메일 인증을 해주세요.");
+        return ResponseEntity.status(ResponseCode.SIGN_UP_SUCCESS.getHttpStatus())
+            .body(ResponseCode.SIGN_UP_SUCCESS.getMessage());
     }
 
     @ApiOperation(value = "관리자 이메일 인증")
     @GetMapping("/manager/verify")
-    public ResponseEntity<String> managerVerifyEmail(
+    public ResponseEntity<?> managerVerifyEmail(
         @RequestParam @ApiParam(value = "관리자 이메일", example = "abc@example.com") String email,
         @RequestParam @ApiParam(value = "이메일 인증코드", example = "abc0123ABC") String code) {
         managerSignUpService.verifyEmail(email, code);
-        return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
+        return ResponseEntity.status(ResponseCode.VERIFICATION_COMPLETED.getHttpStatus())
+            .body(ResponseCode.VERIFICATION_COMPLETED.getMessage());
     }
 
     @ApiOperation(value = "관리자 이메일 인증코드 재발급")
     @GetMapping("/manager/verify/reissue")
-    public ResponseEntity<String> reissueVerificationCode(
+    public ResponseEntity<?> reissueVerificationCode(
         @RequestParam @ApiParam(value = "관리자 아이디", example = "1") Long id) {
         managerSignUpService.reissueVerificationCode(id);
-        return ResponseEntity.ok("이메일 인증코드가 재발급되었습니다.");
+        return ResponseEntity.status(ResponseCode.VERIFICATION_CODE_REISSUED.getHttpStatus())
+            .body(ResponseCode.VERIFICATION_CODE_REISSUED.getMessage());
     }
 
 }
